f3b6412360b103383e1021ee0b9c9a1bfbcb205c
Andrew Lekarev <fl450@mail.ru>
2023-02-26 13:13:28 +0400

    Добавил stylelint

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard-with-typescript'
+    ],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    overrides: [
+        {
+            files: ['*.jsx', '*.tsx'],
+            rules: {
+                '@typescript-eslint/explicit-function-return-type': 0
+            }
+        }
+    ],
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json'
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint'
+    ],
+    rules: {
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        '@typescript-eslint/indent': [2, 4],
+        indent: [2, 4],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'import/no-unresolved': 0,
+        'import/prefer-default-export': 0,
+        'no-unused-vars': 'warn',
+        'react/require-default-props': 0,
+        'react/react-in-jsx-scope': 0,
+        'react/jsx-props-no-spreading': 0,
+        'react/function-component-definition': 0,
+        'no-shadow': 0,
+        'import/extensions': 0,
+        'import/no-extraneous-dependencies': 0,
+        'no-underscore-dangle': 0,
+        '@typescript-eslint/naming-convention': 0,
+        '@typescript-eslint/strict-boolean-expressions': 0,
+        '@typescript-eslint/prefer-nullish-coalescing': 0
+    },
+    globals: {
+        __IS_DEV__: true
+    }
+}
