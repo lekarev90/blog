f8484e559f122493a90eb0cee9f90ec1862b3647
Andrew Lekarev <fl450@mail.ru>
2024-02-11 18:17:44 +0100

    delete screenshots

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -6,10 +6,6 @@ import { LoginForm } from 'features/AuthByUsername/ui/LoginForm/LoginForm';
 export default {
   title: 'features/LoginForm',
   component: LoginForm,
-  args: {
-    isOpen: true,
-    children: 'text text text text text text text text text text text text text text text text text text ',
-  },
 } as ComponentMeta<typeof LoginForm>;
 
 export const Default: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args} />;
