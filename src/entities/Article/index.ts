711b6aa784a6ad8b1f54404d5eccbc2a7d6661bb
Andrew Lekarev <fl450@mail.ru>
2024-05-10 23:29:00 +0200

    fix types error

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,7 +1,7 @@
 export { EArticleTypes } from './model/types/article';
 export { type IArticleDetailsCommentsSchema } from './model/types/articleDetailsCommentsSchema';
 export { type IArticleDetailsSchema } from './model/types/articleDetailsSchema';
-export type { IArticle, EArticlesView } from './model/types/article';
+export { type IArticle, EArticlesView } from './model/types/article';
 export { ArticleRoot } from './ui/ArticleRoot/ArticleRoot';
 export { articlesMockWithRandom } from './model/mock/articlesMock';
 export { ARTICLES_REQUEST_URL } from './model/const/const';
