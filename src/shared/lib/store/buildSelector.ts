2ca0b4b49ecb13037ad1dac45860071705939940
Andrew Lekarev <fl450@mail.ru>
2024-06-04 19:08:02 +0200

    add parameters and types on buildSelector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,11 +1,12 @@
 import { useSelector } from 'react-redux';
 import { IStateSchema } from '@/app/providers/StoreProvider';
 
-type Selector<T> = (state: IStateSchema) => T
-type Result<T> = [() => T, Selector<T>]
+type Selector<T, Args extends any[]> = (state: IStateSchema, ...args: Args) => T;
+type Hook<T, Args extends any[]> = (...args: Args) => T;
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>];
 
-export const buildSelector = <T>(selector: Selector<T>): Result<T> => {
-  const useSelectorHook = () => useSelector(selector);
+export const buildSelector = <T, Args extends any[]>(selector: Selector<T, Args>): Result<T, Args> => {
+  const useSelectorHook: Hook<T, Args> = (...args: Args) => useSelector((state: IStateSchema) => selector(state, ...args));
 
   return [useSelectorHook, selector];
 };
